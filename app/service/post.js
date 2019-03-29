e59957462afabb7bc11ff89db57fc2d4ac6cac31
杨小刚 <nulijiuhao712@126.com>
2019-03-29 10:18:21 +0800

    fix bug

--- app/service/post.js
+++ app/service/post.js
@@ -33,7 +33,7 @@ class PostService extends Service {
     async queryDetail(postId = '') {
         const { app } = this
         const queryPostDetailSql = `
-            SELECT post.title, post.detail, post.date user.name FROM post LEFT JOIN user ON user.id = post.owner_id WHERE post.id = ${postId};
+            SELECT post.title, post.detail, post.date, user.name FROM post LEFT JOIN user ON user.id = post.owner_id WHERE post.id = ${postId};
         `
         //SELECT title, detail, date, (SELECT name FROM user WHERE post.id = ${postId}) as name from post WHERE post.id = ${ postId};
         const res = await app.mysql.query(queryPostDetailSql)
